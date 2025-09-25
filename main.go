034d6948404e3279c0a061a5563d1b19391a7190
hghomari <hamzaghomari16@gmail.com>
2025-09-25 14:44:22 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var (
+	number_of_ants = -1
+	first          = false
+	the_rooms      = make(map[string][]string)
+	visit          = make(map[string]bool)
+	RestOfAnt      = 0
+	tunnels        = [][]string{}
+	Enter_Ant      = []string{}
+)
+
+func Ant_Path(start, end string) {
+	path := []string{}
+	Find_Path(start, end, path)
+}
+
+func Find_Path(current, end string, path []string) {
+	path = append(path, current)
+	visit[current] = true
+
+	tunnels = append(tunnels, path)
+
+	for _, room := range the_rooms[current] {
+		if !visit[room] {
+			Find_Path(room, end, path)
+		}
+		for room2, room3 := range the_rooms {
+			for _, inside := range room3 {
+				if inside == room {
+					if !visit[room2] {
+						Find_Path(room2, end, path)
+					}
+				}
+			}
+		}
+		
+	}
+}
+
+func Check_Char(str string, char rune) int {
+	for i, c := range str {
+		if char == c {
+			return i
+		}
+	}
+	return -1
+}
+
+func Relation_Room(Firstroom, neighbors string) {
+	RoomExist := false
+	for room, neighbor := range the_rooms {
+		if room == Firstroom {
+			RoomExist = true
+			neighbor = append(neighbor, neighbors)
+			the_rooms[Firstroom] = neighbor
+		}
+	}
+	if !RoomExist {
+		var neighbor []string
+		neighbor = append(neighbor, neighbors)
+		the_rooms[Firstroom] = neighbor
+	}
+}
+
+func main() {
+	args := os.Args[1:]
+	if len(args) == 1 {
+		if strings.HasSuffix(args[0], ".txt") {
+			if _, err := os.Stat(args[0]); os.IsNotExist(err) {
+				_, err = os.Create(args[0])
+				if err != nil {
+					fmt.Println("Error: ", err)
+					return
+				}
+			}
+		} else {
+			fmt.Println("you have to write command like this : go run . exfile.txt")
+			return
+		}
+		file, err := os.Open(args[0])
+		if err != nil {
+			fmt.Println("Error: ", err)
+			return
+		}
+		defer file.Close()
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			text := scanner.Text()
+			if number_of_ants == -1 {
+				number_of_ants, err = strconv.Atoi(text)
+				if err != nil {
+					fmt.Println("Error: ", err)
+					return
+				}
+				fmt.Println(text)
+			} else if text == "##end" || text == "##start" {
+				fmt.Println(text)
+				first = true
+			} else if first {
+				i := Check_Char(text, '-')
+				if i != -1 {
+					Relation_Room(string(text[:i]), string(text[i+1:]))
+				}
+				fmt.Println(text)
+			}
+		}
+		for key, strr := range the_rooms {
+			fmt.Printf("%s-%s ", key, strr)
+		}
+		Ant_Path("1", "0")
+		fmt.Println()
+		for _, path := range tunnels {
+			fmt.Print(path)
+		}
+		if err := scanner.Err(); err != nil {
+			fmt.Println("Error reading file:", err)
+			return
+		}
+	}
+}

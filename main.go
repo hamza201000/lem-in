a76c459f26c2a0dc77133146654132cca2864b67
hamza201000 <HAMZAGHOMARI700@gmail.com>
2025-10-25 23:18:02 +0200

    Include all rooms in graph parsing
    Added variable to store all rooms parsed from the file.

--- main.go
+++ main.go
@@ -14,11 +14,12 @@ func main() {
 		return
 	}
 
-	graph, err := funchandler.ParseFileToGraph(args[0])
+	graph, allrooms, err := funchandler.ParseFileToGraph(args[0])
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	
 
 	// set global ants and start/end names
 
@@ -28,13 +29,24 @@ func main() {
 	}
 
 	// now run algorithm
-	fmt.Println(graph.Ants)
-	fmt.Println(graph.Start)
+	
+	
 	Base_Tunnels := funchandler.Get_All_Path(graph.Start.Name, graph.End.Name, graph.The_rooms, (funchandler.Init_Path_Groups(graph.Start.Name, graph.End.Name, graph.The_rooms)))
-	fmt.Println(Base_Tunnels)
+	if len(Base_Tunnels)==0{
+		fmt.Println("ERROR: start or end not set by parser")
+		return
+	}
+	fmt.Println(graph.Ants)
+	for _, lines := range allrooms {
+		for _, line := range lines {
+			fmt.Println(line)
+		}
+	}
+	fmt.Println()
 	Tunnels, Indx_Grp, Turn := funchandler.Find_Best_Group(graph.Ants, funchandler.CopySlice(Base_Tunnels))
 
 	Bs_Tunnel := Base_Tunnels[Indx_Grp]
-
+	
+	
 	funchandler.Move_Ant(graph.Ants, graph.End.Name, Tunnels, Bs_Tunnel, Turn)
 }
